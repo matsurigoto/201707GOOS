a7b7fd25deecf4da8a4c733d2e365baac7be0316
duran.hsieh <duran.hsieh@afusion.com>
2017-07-01 13:12:38 +0800

    add Hooks.cs

---
+++ GOOS_SampleTests/Hooks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAutomation;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+
+        [BeforeFeature()]
+        [Scope(Tag = "web")]
+        public static void SetBrowser()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
--- GOOS_SampleTests/features/BudgetCreate.feature
+++ GOOS_SampleTests/features/BudgetCreate.feature
@@ -1,6 +1,7 @@
-﻿Feature: BudgetCreate
+﻿
+@web
+Feature: BudgetCreate
 
-@mytag
 Scenario: Add a budget successfully
     Given go to adding budget page
     When I add a buget 2000 for "2017-10"
